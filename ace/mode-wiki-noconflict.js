6d06102af67c76ba0725531d6c2b6399b7a864ef
Dan Michael O. Heggø <danmichaelo@gmail.com>
2012-03-31 18:12:19 +0200

    adding files

---
+++ ace/mode-wiki-noconflict.js
@@ -0,0 +1,137 @@
+
+
+ace.define('ace/mode/wiki', function(require, exports, module) {
+
+  var oop = require("ace/lib/oop");
+  var TextMode = require("ace/mode/text").Mode;
+  var Tokenizer = require("ace/tokenizer").Tokenizer;
+  var WikiHighlightRules = require("ace/mode/wiki_highlight_rules").WikiHighlightRules;
+
+  var Mode = function() {
+     this.$tokenizer = new Tokenizer(new WikiHighlightRules().getRules());
+  };
+  oop.inherits(Mode, TextMode);
+  
+  exports.Mode = Mode;
+});
+
+
+
+ace.define('ace/mode/wiki_highlight_rules', function(require, exports, module) {
+
+  var oop = require("ace/lib/oop");
+  var TextHighlightRules = require("ace/mode/text_highlight_rules").TextHighlightRules;
+
+  var WikiHighlightRules = function() {
+
+    // regexp must not have capturing parentheses
+    // regexps are ordered -> the first match is used
+    this.$rules = {
+        start : [
+            /*{
+                // Curly and square braces
+                token : "lparen",
+                regex : "[[({]"
+            }, {
+                // Curly and square braces
+                token : "rparen",
+                regex : "[\\])}]"
+            }, 
+             {
+                // Templates {{ ... }}
+                token : "template",
+                regex : "{{[^}]*}}"
+            },*/
+            {
+                token : "comment",
+                merge : true,
+                regex : "<\\!--",
+                next : "comment"
+            }, {
+                // External links [ ... ]
+                token : "externallink",
+                regex : "\\[[^\\[\\]]*\\]"
+            }, { // headings
+                token: "markup.heading.1",
+                regex: "^==.*==\\s*$"
+            }, {
+                token : "meta.tag",
+                regex : "<.*?>"
+            }, {
+                token : "table",
+                merge : true,
+                regex : "{\\|",
+                next : "table"
+            }, {
+                token : "template",
+                merge : true,
+                regex : "{{",
+                next : "template"
+            }, {
+                // Wikilinks {{ ... }}
+                token : "wikilinkbraces",
+                regex : "\\[\\[",
+                next : "wikilink"
+            }],
+        wikilink : [
+         {
+                token : "wikilinkbraces",
+                regex : "\\]\\]",
+                next : "start"
+            }, {
+                token : "wikilink",
+                merge : true,
+                regex : "[^\\]]+"
+            }],
+        comment : [ 
+            {
+                token : "comment",
+                regex : ".*?-->",
+                next : "start"
+            }, {
+                token : "comment",
+                merge : true,
+                regex : ".+"
+            }],
+        table : [ 
+            {
+                token : "table",
+                regex : ".*?\\|}",
+                next : "start"
+            }, {
+                token : "table",
+                merge : true,
+                regex : ".+"
+            }], 
+        template : [ 
+            {
+                token : "template.sub",
+                merge : true,
+                regex : "{{",
+                next : "subtemplate"
+            }, {
+                token : "template",
+                regex : "}}",
+                next : "start"
+            }, {
+                token : "template",
+                merge : true,
+                regex : "[^{}]+"
+            }],
+        subtemplate : [ 
+            {
+                token : "template.sub",
+                regex : ".*?}}",
+                next : "template"
+            }, {
+                token : "template.sub",
+                merge : true,
+                regex : ".+"
+            }]
+    };
+
+  }
+
+  oop.inherits(WikiHighlightRules, TextHighlightRules);
+  exports.WikiHighlightRules = WikiHighlightRules;
+});
